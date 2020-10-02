33b0e68419f69c0e33a83c65a53dd52f0bcb6ed3
Ryan Au <auryan898@gmail.com>
2020-10-01 23:44:51 -0400

    Demo for Heroku setup

--- VisArtApplication/src/main/java/com/ecse321/visart/DemoApplication.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/DemoApplication.java
@@ -1,9 +1,21 @@
 package com.ecse321.visart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
-    System.out.println("Hello from Spring Boot!");
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "Hello World!";
   }
 
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
