d08e5c16a86ee52fba3f65e4ceebb25b5a73d137
Mahin Anwar <anwar@Mahins-MacBook-Pro.local>
2020-10-09 14:20:36 -0400

    add user test

--- VisArtApplication/src/main/java/com/ecse321/visart/repositories/UserRepository.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/repositories/UserRepository.java
@@ -1,24 +1,59 @@
+
+
+
 package com.ecse321.visart.repositories;
 
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ecse321.visart.model.Gallery;
+import com.ecse321.visart.model.Manager;
 import com.ecse321.visart.model.User;
+import com.ecse321.visart.model.UserRole;
 
 @Repository
 public class UserRepository {
+    
+  @Autowired
+  EntityManager entityManager;
+  
+  @Transactional
+  public User createUser(String aIdCode, String aEmailAddress, String aDisplayname, String aUsername, String aPassword, String aProfilePicLink, String aProfileDescription ) {
+    User usr = new User(aIdCode, aEmailAddress, aDisplayname, aUsername, aPassword, aProfilePicLink, aProfileDescription );
+    //Manager manager = new Manager(aIdCode, usr);
+    entityManager.persist(usr);
+   // entityManager.persist(manager);
+    return usr;
+  }
+  
+  @Transactional 
+  public User getUser(String aIdCode) {
+    return entityManager.find(User.class, aIdCode);
+  }
+  
+  @Transactional
+  public void updateUser(User usr) {
+    entityManager.merge(usr);
+    //entityManager.merge(usr.getUser());
+  }
+  
+  @Transactional 
+  public boolean deleteUser(User usr) {
+    User entity = entityManager.find(User.class, usr.getIdCode());
+    //User usr = entityManager.find(User.class, manager.getIdCode());
+    if(entityManager.contains(entity)){
+      entityManager.remove(entityManager.merge(entity));
+      //entityManager.remove(entityManager.merge(usr));
+    } else {
+      entityManager.remove(entity);
+      //entityManager.remove(usr);
+    }
+    
+    return !entityManager.contains(entity); //&& !entityManager.contains(usr);
+  }
 
-	@Autowired
-	EntityManager entityManager;
-	
-	@Transactional
-	public User getUser(String idCode) {
-		return entityManager.find(User.class, idCode);
-	}
-	
-	
 }
---
+++ VisArtApplication/src/test/java/com/ecse321/visart/test/UserTest.java
@@ -0,0 +1,120 @@
+package com.ecse321.visart.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.ecse321.visart.model.User;
+import com.ecse321.visart.repositories.UserRepository;
+
+@TestMethodOrder(OrderAnnotation.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class UserTest {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	private String email;
+
+	private String displayName;
+
+	private String username;
+
+	private String password;
+	
+	private static String profilePic;
+	  
+	private static String profileDesc;
+
+	static private User testManager2;
+	
+
+	// Pseudo-random ID for tested entities
+	static Long l = System.currentTimeMillis();
+
+	@Test
+	@Order(1)
+	void init() {
+		// Attributes
+		email = "blah";
+		displayName = "Riad";
+		username = "riadelm";
+		password = "aNicePassword";
+		profilePic = "Bezos.jpg";
+	    profileDesc = "ACAB, billionaires shouldnt exist, capitalism suxx";
+	}
+
+	@Test
+	@Order(2)
+	void testCreate1() {
+		// Create
+		User testUser = userRepo.createUser("" + l, email, displayName, username, password, profilePic, profileDesc);
+
+		// TEST if Manager was created
+		assertNotNull(testUser);
+
+		// Print Manager
+		System.out.println("=================CREATE===============");
+		System.out.println(testUser);
+		System.out.println("=================CREATE===============");
+	}
+/*
+	@Test
+	@Order(3)
+	void testGet1() {
+		// Find manager
+		testManager2 = managerRepo.getManager("" + l);
+
+		// TEST if Manager was retrieved
+		assertNotNull(testManager2);
+		assertEquals(email, testManager2.getUser().getEmailAddress());
+		assertEquals(displayName, testManager2.getUser().getDisplayname());
+		assertEquals(username, testManager2.getUser().getUsername());
+		assertEquals(password, testManager2.getUser().getPassword());
+
+		System.out.println("=================FIND===============");
+		System.out.println(testManager2);
+		System.out.println("=================FIND===============");
+	}
+
+	@Test
+	@Order(4)
+	void testUpdate1() {
+		// Find manager
+		testManager2 = managerRepo.getManager("" + l);
+		
+		//Update Manager
+		testManager2.getUser().setDisplayname("newDisplayName");
+		managerRepo.updateManager(testManager2);
+		
+		testManager2 = managerRepo.getManager(""+l);
+
+		// TEST if Manager was retrieved and if properly updated
+		assertNotNull(testManager2);
+		assertEquals(email, testManager2.getUser().getEmailAddress());
+		assertEquals("newDisplayName", testManager2.getUser().getDisplayname());
+		assertEquals(username, testManager2.getUser().getUsername());
+		assertEquals(password, testManager2.getUser().getPassword());
+
+		System.out.println("=================UPDATE===============");
+		System.out.println(testManager2);
+		System.out.println("=================UPDATE===============");
+	}
+
+	@Test
+	@Order(5)
+	void testDelete1() {
+		managerRepo.deleteManager(testManager2);
+		assertEquals(null, managerRepo.getManager("" + l));
+	} */
+
+}
