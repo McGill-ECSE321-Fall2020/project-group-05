9965bb9a44191aac914559a3a79c26692b6a2289
riadelm <elmahmoudy.riad@gmail.com>
2020-10-19 20:18:14 -0400

    use dtos instead of entity classes

--- VisArtApplication/src/main/java/com/ecse321/visart/dto/TagDto.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/dto/TagDto.java
@@ -1,6 +1,26 @@
 package com.ecse321.visart.dto;
 
+import com.ecse321.visart.model.ArtListing;
+import com.ecse321.visart.model.Tag.TagType;
+
 public class TagDto {
+	private String keyword;
+	private String tagIdCode;
+	private TagType tagType;
+	private ArtListingDto tagListing;
+	
+	//constructor
+	public TagDto(String keyword, String tagIdCode, TagType tagType, ArtListing tagListing) {
+		super();
+		this.keyword = keyword;
+		this.tagIdCode = tagIdCode;
+		this.tagType = tagType;
+		this.tagListing = tagListing;
+		
+		
+	}
+	
+	
 	
 	
 	
