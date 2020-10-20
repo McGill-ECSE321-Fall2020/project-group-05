56dbcf6ce38b26e17d0c433855e8ef34cfb62c35
riadelm <elmahmoudy.riad@gmail.com>
2020-10-19 20:32:31 -0400

    added ticketDto constructor

--- VisArtApplication/src/main/java/com/ecse321/visart/dto/TicketDto.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/dto/TicketDto.java
@@ -9,5 +9,17 @@ public class TicketDto {
 	private CustomerDto ticketCustomer;
 	private ArtistDto ticketArtist;
 	
+	//Constructor
+	public TicketDto(boolean isPaymentConfirmed, double paymentAmount, String ticketIdCode, ArtOrderDto ticketOrder,
+			CustomerDto ticketCustomer, ArtistDto ticketArtist) {
+		super();
+		this.isPaymentConfirmed = isPaymentConfirmed;
+		this.paymentAmount = paymentAmount;
+		this.ticketIdCode = ticketIdCode;
+		this.ticketOrder = ticketOrder;
+		this.ticketCustomer = ticketCustomer;
+		this.ticketArtist = ticketArtist;
+	}
+	
 	
 }
