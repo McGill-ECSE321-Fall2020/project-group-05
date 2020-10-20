43847c726817b4e0f689e0598da86b6930d6d9a2
riadelm <elmahmoudy.riad@gmail.com>
2020-10-19 20:31:29 -0400

    created ticketDto class

--- VisArtApplication/src/main/java/com/ecse321/visart/dto/TicketDto.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/dto/TicketDto.java
@@ -1,5 +1,13 @@
 package com.ecse321.visart.dto;
 
 public class TicketDto {
-
+   //Private fields
+	private boolean isPaymentConfirmed;
+	private double paymentAmount;
+	private String ticketIdCode;
+	private ArtOrderDto ticketOrder;
+	private CustomerDto ticketCustomer;
+	private ArtistDto ticketArtist;
+	
+	
 }
