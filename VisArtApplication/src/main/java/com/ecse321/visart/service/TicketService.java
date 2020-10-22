9b171fcd16cbad731ac0ac4fe5f21b7df9095475
riadelm <elmahmoudy.riad@gmail.com>
2020-10-21 21:26:38 -0400

    added createticket method to ticket repo

--- VisArtApplication/src/main/java/com/ecse321/visart/service/TicketService.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/service/TicketService.java
@@ -3,6 +3,25 @@
  */
 package com.ecse321.visart.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ecse321.visart.model.ArtOrder;
+import com.ecse321.visart.model.Artist;
+import com.ecse321.visart.model.Customer;
+import com.ecse321.visart.model.Ticket;
+import com.ecse321.visart.repositories.TicketRepository;
+
 public class TicketService {
+	
+	@Autowired
+	TicketRepository ticketRepo;
+	
+	public Ticket createTicket(boolean aIsPaymentConfirmed, double aPaymentAmount, String aIdCode,
+		      ArtOrder aOrder, Customer aCustomer, Artist aArtist){
+		if(aIdCode == null || aIdCode == "") {
+			throw new IllegalArgumentException("User id code cannot be empty!");
+		}
+		return ticketRepo.createTicket(aIsPaymentConfirmed, aPaymentAmount, aIdCode, aOrder, aCustomer, aArtist);
+	}
 
 }
