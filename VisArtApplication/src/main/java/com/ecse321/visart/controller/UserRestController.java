f60d70523227e9f6eacc4df3ac7fd23fe233d145
Daniel Bucci <danielbucci2@gmail.com>
2020-10-31 16:51:47 -0400

    creates User and Manager tests

--- VisArtApplication/src/main/java/com/ecse321/visart/controller/UserRestController.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/controller/UserRestController.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecse321.visart.dto.ManagerDto;
 import com.ecse321.visart.dto.UserDto;
 import com.ecse321.visart.service.UserService;
 
@@ -26,7 +29,11 @@ public class UserRestController {
   }
 
   @PostMapping(value = { "/users/{name}", "/users/{name}/" })
-  public UserDto createUser(@PathVariable("name") String name) {
-    return new UserDto(service.createUser(name, "", "", "", "", "", ""));
+  public UserDto createUser(@RequestBody MultiValueMap<String, String> values) {
+    return new UserDto(service.createUser(values.getFirst("idCode"), 
+        values.getFirst("emailAddress"), values.getFirst("displayName"),
+        values.getFirst("userName"), values.getFirst("password"),
+        values.getFirst("profilePicLink"), values.getFirst("profileDescription")));
   }
+  
 }
--- VisArtApplication/src/main/java/com/ecse321/visart/service/ManagerService.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/service/ManagerService.java
@@ -1,11 +1,13 @@
 package com.ecse321.visart.service;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ecse321.visart.model.ArtListing;
 import com.ecse321.visart.model.Manager;
 import com.ecse321.visart.model.User;
 import com.ecse321.visart.repositories.EntityRepository;
@@ -27,12 +29,55 @@ public class ManagerService {
     if (aIdCode == null || aIdCode == "") {
       throw new IllegalArgumentException("Manager id code cannot be empty!");
     }
-
+    
+    if(isValidEmail(aEmailAddress)==false) {
+      throw new IllegalArgumentException("Email address is invalid");
+    }
+    
+    if(aDisplayname.length()<5 || aDisplayname.length()>25) {
+      throw new IllegalArgumentException("This Display Name is invalid, must be between 5 and 25 characters!");
+    }
+    
+    List<Manager> l1 = entityRepo.findEntityByAttribute("displayname",Manager.class,aDisplayname);
+    if (l1 != null && l1.size() > 0) {
+        throw new IllegalArgumentException("This Display Name is already taken!");
+    }
+    
+    if(aUsername.length()<5 || aUsername.length()>25 ) {
+      throw new IllegalArgumentException("This User Name is invalid, must be between 5 and 25 characters!");
+    }
+    
+    List<Manager> l2 = entityRepo.findEntityByAttribute("username",Manager.class,aUsername);
+    if (l2 != null && l2.size() > 0) {
+        throw new IllegalArgumentException("This Username is already taken!");
+    }
+    
+    if(aPassword.length()<8 || aPassword.length()>40) {
+      throw new IllegalArgumentException("Password must be between 8 and 40 characters");
+    }
+    
+    if(aProfileDescription.length()>255) {
+      throw new IllegalArgumentException("Description must be less than 255 characters");
+    }
+    
+    String hashedPassword;
+    
     return managerRepo.createManager(aIdCode, aEmailAddress, aDisplayname, aUsername, aPassword,
         aProfilePicLink, aProfileDescription);
 
   }
-
+  
+//  @Transactional
+//  public Manager addManagerListing(String aIdCode, ArtListing artListing) {
+//    Manager manager = managerRepo.getManager(aIdCode, true);
+//    if(artListing != null) {
+//      manager.addPromotedListing(artListing);
+//    } else {
+//      throw new IllegalArgumentException("Cannot add a null art listing");
+//    }
+//    return manager;
+//  }
+  
   @Transactional
   public Manager getManager(String aIdCode) {
     return managerRepo.getManager(aIdCode);
@@ -42,10 +87,32 @@ public class ManagerService {
   public Manager getManager(String aIdCode, Boolean promotedListings) {
     return managerRepo.getManager(aIdCode, promotedListings);
   }
+  
+  @Transactional
+  public Boolean deleteManager(String aIdCode) {
+    if(aIdCode != null && !aIdCode.contentEquals("")) {
+      return managerRepo.deleteManager(aIdCode);
+    } else {
+      return false;
+    }
+  }
 
   @Transactional
   public List<Manager> getAllUsers() {
     return entityRepo.getAllEntities(Manager.class);
   }
+  
+  //https://www.geeksforgeeks.org/check-email-address-valid-not-java/
+  public static boolean isValidEmail(String email) { 
+      String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                          "[a-zA-Z0-9_+&*-]+)*@" + 
+                          "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                          "A-Z]{2,7}$"; 
+                            
+      Pattern pat = Pattern.compile(emailRegex); 
+      if (email == null) 
+          return false; 
+      return pat.matcher(email).matches(); 
+  } 
 
 }
