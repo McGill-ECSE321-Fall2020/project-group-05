ca25564a3f22b1167fd6a132e57406ab5785bdb1
Daniel Bucci <danielbucci2@gmail.com>
2020-10-31 16:58:40 -0400

    User Rest Controller

--- VisArtApplication/src/main/java/com/ecse321/visart/controller/UserRestController.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/controller/UserRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecse321.visart.dto.ManagerDto;
@@ -27,6 +28,11 @@ public class UserRestController {
   public List<UserDto> getAllUsers() {
     return service.getAllUsers().stream().map(u -> new UserDto(u)).collect(Collectors.toList());
   }
+  
+  @GetMapping(value = { "/get_user", "/get_user/" })
+  public UserDto getUser(@RequestParam("idCode") String aIdCode) {
+    return new UserDto(service.getUser(aIdCode));
+  }
 
   @PostMapping(value = { "/users/{name}", "/users/{name}/" })
   public UserDto createUser(@RequestBody MultiValueMap<String, String> values) {
@@ -35,5 +41,20 @@ public class UserRestController {
         values.getFirst("userName"), values.getFirst("password"),
         values.getFirst("profilePicLink"), values.getFirst("profileDescription")));
   }
+  
+  @PostMapping(value = { "/users_update/{name}", "/users_update/{name}/" })
+  public UserDto updateUser(@RequestBody MultiValueMap<String, String> values) {
+    return new UserDto(service.updateUser(values.getFirst("idCode"), 
+        values.getFirst("emailAddress"), values.getFirst("displayName"),
+        values.getFirst("userName"), values.getFirst("password"),
+        values.getFirst("profilePicLink"), values.getFirst("profileDescription")));
+  }
+  
+  @PostMapping(value = {"/delete_user/{id}","delete_user/{id}/"})
+  public Boolean deleteUser(@PathVariable("id") String idCode) {
+    return service.deleteUser(idCode);
+  }
+  
+  
   
 }
