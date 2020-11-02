e8c3af9cff8f6bbcc74f5957e804075da4e3221f
Ryan Au <auryan898@gmail.com>
2020-11-02 02:40:18 -0500

    Add subproject for http requests for integrations

--- VisArtApplication/integrationTest.bat
+++ VisArtApplication/integrationTest.bat
@@ -1,27 +1,14 @@
 ECHO off
 
+::Using a custom java jar to create http requests ->
+::java -jar ..\JavaHttpRequests\build\libs\JavaHttpRequests.jar POST google.com / {"pie":"data"}
+set "http=java -jar ..\JavaHttpRequests\build\libs\JavaHttpRequests.jar"
 
-:: A basic curl request ->
-:: curl -s -X GET http://localhost:8080/managers/
-:: OR THIS FOR POST ->
-:: curl -s -X POST http://localhost:8000/managers/ {"name":"bob"}
-
-:: This is an assertion example ->
-:: curl -s -X GET http://localhost:8080/ | findstr "string to find"
-:: if not errorlevel 1 (
-::     echo success: connects to localhost:8080
-:: ) else (
-::     echo failed: localhost:8080 not available
-:: )
-
-curl -s -X GET http://localhost:8080/ | findstr "Vis"
+%http% GET localhost:8080 /
 if not errorlevel 1 (
     echo success: connects to localhost:8080
 ) else (
     echo failed: localhost:8080 not available
 )
 
-curl -s -X POST http://localhost:8080/create_manager --data '{"idCode":"16324123323423","emailAddress":"auryan898@gmail.com","displayName":"dingdong","userName":"hiIAmBilly","ProfilePicLink":"","profileDescription":""}'
-
-
-echo on
+ECHO on
