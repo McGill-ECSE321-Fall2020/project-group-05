8390f8ddf4d4cac1b92733b7fea9c9456a505931
Ryan Au <auryan898@gmail.com>
2020-11-02 20:55:37 -0500

    Fix travis config, remove old scripts

--- VisArtApplication/integrationTest.sh
+++
@@ -1,8 +0,0 @@
-curl -s -X GET http://localhost:8080/
-if [ $? -eq 0 ]
-then
-  echo "success: connects to localhost:8080"
-else
-  echo "failure: cannot connect to localhost"
-  curl -s -X GET https://vis-art-application.herokuapp.com/
-fi
