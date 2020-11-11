8651f9131b0671e2136bea08d8e237de715be2b7
riadelm <elmahmoudy.riad@gmail.com>
2020-11-11 15:28:11 -0500

    added hooper slides and fixed more front end stuff

--- visart-web-app/src/views/Home.vue
+++ visart-web-app/src/views/Home.vue
@@ -3,18 +3,57 @@
   <div class="home">
   <button type="button" class="btn btn-primary" id="fixedbutton">+</button>
   <section id="mainArtSection">
-  <div class="sectionImage" id="mainHomeArt"></div>
+    <hooper id="hooperContainer" :autoPlay="true" :playSpeed="70000" itemsToShow="1">
+    <slide>
+      <div class="sectionImage" id="mainHomeArt1"></div>
+    </slide>
+    <slide>
+      <div class="sectionImage" id="mainHomeArt2"></div>
+    </slide>
+    <slide>
+      <div class="sectionImage" id="mainHomeArt3"></div>
+    </slide>
+    <slide>
+      <div class="sectionImage" id="mainHomeArt4"></div>
+    </slide>
+    </hooper>
   <div class="sectionContent" id="sectionContentTitle">Vis Art</div>
 </section>
 <section id="tagSection">
-<button type="button" class="btn btn-primary tagBtn">Acrylic</button>
-<button type="button" class="btn btn-primary tagBtn">Sculpture</button>
-<button type="button" class="btn btn-primary tagBtn">Cubism</button>
-<button type="button" class="btn btn-primary tagBtn">Contemporary</button>
-<button type="button" class="btn btn-primary tagBtn">Mixed</button>
-<button type="button" class="btn btn-primary tagBtn">Realism</button>
-<button type="button" class="btn btn-primary tagBtn">Fauvism</button>
-<button type="button" class="btn btn-primary tagBtn">Oil</button>
+<div id="tagContainer">
+ <hooper :settings="hooperSettings">
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Acrylic</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Sculpture</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Cubism</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Realism</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Fauvism</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Oil</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Absurd</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Comtemporary</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Resin</button>
+    </slide>
+    <slide>
+    <button type="button" class="btn btn-primary tagBtn">Glass</button>
+    </slide>
+  </hooper>
+</div>
 </section>
     <div class="listingContainer">
       <h1> – Featured Art – </h1>
@@ -81,10 +120,26 @@
 </template>
 <script>
 // @ is an alias to /src
-
+//  bar scroll
+import { Hooper, Slide } from 'hooper'
+import 'hooper/dist/hooper.css'
 export default {
   name: 'Home',
   components: {
+    Hooper,
+    Slide
+  },
+  data () {
+    return {
+      hooperSettings: {
+        itemsToShow: 8,
+        centerMode: true,
+        infiniteScroll: true,
+        itemsToSlide: 8,
+        rtl: true,
+        pagination: 'no'
+      }
+    }
   }
 }
 </script>
