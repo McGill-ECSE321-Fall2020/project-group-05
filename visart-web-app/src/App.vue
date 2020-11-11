709f87950877d26e57724a68f8ecca2a128cec95
riadelm <elmahmoudy.riad@gmail.com>
2020-11-10 23:08:41 -0500

    created skeleton for main page

--- visart-web-app/src/App.vue
+++ visart-web-app/src/App.vue
@@ -9,6 +9,97 @@
   </div>
 </template>
   <style>
+.mainArtContainer{
+  height: 532px;
+}
+.mainArt{
+  background-size: 100% auto;
+}
+#mainArtSection{
+  background:#444;
+  position:relative;
+  margin: auto;
+  height:385px;
+  width:100%;
+  color:#fff;
+}
+#tagSection{
+  background:rgb(146, 142, 131);
+  position:relative;
+  margin: auto;
+  height:60px;
+  width:100%;
+  color:#fff;
+}
+.sectionImage{
+  width: 100%;
+  height:100%;
+  background: transparent none no-repeat top top;
+  background-size: cover;
+}
+.sectionContent{
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#sectionContentTitle{
+    font-size: 170px;
+    top: 50%;
+}
+#sectionContentListing{
+    color:rgb(78, 71, 61);
+    font-size:50px;
+    top: 30%;
+    font-weight:400;
+}
+.cardImg{
+  opacity: 0.4;
+  filter: alpha(opacity=40); /* msie */
+}
+.cardImg:hover{
+  opacity: 1;
+}
+#fixedbutton {
+    position: fixed;
+    bottom: 5%;
+    right: 5%;
+    z-index: 9998;
+    background-color: rgb(202, 121, 110);
+    font-size:200%;
+    font-weight: 600;
+    border-radius: 50%;
+    height: 70px;
+    width: 70px;
+    border-width: 0px;
+}
+#mainHomeArt{
+  background-image: url(https://www.slantmagazine.com/wp-content/uploads/2010/05/picassoandbraquegotothemovies.jpg);
+}
+.tagBtn{
+  margin-top:10px;
+  margin-bottom:10px;
+  margin-left: 5px;
+  margin-right: 5px;
+  padding:0px;
+  height:40px;
+  width:150px;
+  border-color:rgb(189, 175, 154) !important;
+  background-color:rgb(226, 226, 220) !important;
+  color: rgb(153, 153, 131) !important;
+}
+.tagBtn:hover{
+  background-color:rgb(153, 153, 131) !important;
+  color: rgb(226, 226, 220) !important;
+  border-color:rgb(163, 155, 142) !important;
+}
+.listingContainer{
+  padding:75px;
+  background-color:rgb(226, 226, 220);
+}
+.card{
+  width:350px !important;
+  margin:30px !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- visart-web-app/src/views/Home.vue
+++ visart-web-app/src/views/Home.vue
@@ -1,10 +1,74 @@
 <template>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <button type="button" class="btn btn-primary" id="fixedbutton">+</button>
+  <section id="mainArtSection">
+  <div class="sectionImage" id="mainHomeArt"></div>
+  <div class="sectionContent" id="sectionContentTitle">Vis Art</div>
+</section>
+<section id="tagSection">
+<button type="button" class="btn btn-primary tagBtn">Acrylic</button>
+<button type="button" class="btn btn-primary tagBtn">Sculpture</button>
+<button type="button" class="btn btn-primary tagBtn">Cubism</button>
+<button type="button" class="btn btn-primary tagBtn">Contemporary</button>
+<button type="button" class="btn btn-primary tagBtn">Mixed</button>
+<button type="button" class="btn btn-primary tagBtn">Realism</button>
+<button type="button" class="btn btn-primary tagBtn">Fauvism</button>
+<button type="button" class="btn btn-primary tagBtn">Oil</button>
+</section>
+    <div class="listingContainer">
+      <div class="card-columns">
+  <div class="card shadow-sm ">
+    <img class="card-img-top cardImg" src="../assets/cardTrial.png" alt="Card image cap">
+    <div class="sectionContent" id="sectionContentListing">Title of Painting</div>
+    <div class="card-body">
+      <h4 class="card-title">Author</h4>
+      <h5 class="card-title">Price</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+    </div>
+  </div>
+  <div class="card shadow-sm">
+    <img class="card-img-top" src="../assets/cardTrial0.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+      </div>
+  </div>
+  <div class="card shadow-sm">
+    <img class="card-img-top" src="../assets/cardTrial3.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+    </div>
+  </div>
+  <div class="card shadow-sm">
+    <img class="card-img-top" src="../assets/cardTrial2.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+    </div>
+  </div>
+  <div class="card shadow-sm">
+    <img class="card-img-top" src="../assets/cardTrial1.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+    </div>
+  </div>
+  <div class="card shadow-sm">
+    <img class="card-img-top" src="../assets/cardTrial4.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Title</h5>
+      <p class="card-text">This is a description of the painting. The painting is described by this description. The description describes the painting</p>
+    </div>
+  </div>
+</div>
+    </div>
+    <div>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
