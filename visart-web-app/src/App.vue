42d341470d5137739d39390358ad9c3d7d002983
riadelm <elmahmoudy.riad@gmail.com>
2020-11-11 11:22:39 -0500

    fixed navbar

--- visart-web-app/src/App.vue
+++ visart-web-app/src/App.vue
@@ -1,7 +1,7 @@
 
   <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand" href="#"><img id="navlogo" src="./assets/navlogo0.png" alt="nav"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -10,36 +10,39 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="/about">About</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0" id="searchForm">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn my-2 my-sm-0" type="submit"><img id="searchIcon" src="./assets/loupe.png" alt="Search"></button>
     </form>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item loginDiv">
+        <a class="nav-link" href="/about">Login</a>
+      </li>
+      <li class="nav-item loginDiv">
+        <a class="nav-link" href="/about">Sign Up</a>
+      </li>
+    </ul>
   </div>
 </nav>
     <router-view />
   </div>
 </template>
   <style>
+  /*
+  Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+*/
+.loginDiv{
+
+}
+.navbar{
+background-color: rgb(226, 226, 220) !important;
+}
 .mainArtContainer{
   height: 532px;
 }
@@ -149,6 +152,18 @@ padding-bottom:10%;
 }
 .cardHeader{
 display:inline;
+}
+#searchForm{
+position: absolute;
+left: 50%;
+transform: translatex(-50%);
+}
+#searchIcon{
+  transform:scale(0.9,0.9);
+  padding: 0px;
+  margin: 0px;
+  max-height: 30px;
+  max-width: 30px;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
