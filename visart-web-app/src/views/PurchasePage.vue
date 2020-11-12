2dd78b9148f08b8eb87b13c20bd91d236c1c3b84
Ryan Au <auryan898@gmail.com>
2020-11-12 18:13:43 -0500

    Fixed the PurchasePage

--- visart-web-app/src/views/PurchasePage.vue
+++ visart-web-app/src/views/PurchasePage.vue
@@ -1,29 +1,45 @@
 <template>
-<button type="button" class="btn btn-primary" id="fixedbutton">+</button>
-<div class="container container-space">
-  <div class="row">
-    <div class="col-xl">
-      <div class="card" style="width: 35rem;">
-        <img class="card-img-top card-space-image" src=image alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title text-center">{{title}}</h5>
+<span>
+  <button type="button" class="btn btn-primary" id="fixedbutton">+</button>
+  <div class="container container-space">
+    <div class="row">
+      <div class="col-xl">
+        <div class="card" style="width: 35rem;">
+          <img
+            class="card-img-top card-space-image"
+            src="image"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center">{{ title }}</h5>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-sm">
-      <ul class="list-group card-space">
-        <li class="list-group-item list-group-item-secondary">Artist:</li>
-        <li class="list-group-item">{{artistName}}</li>
-        <li class="list-group-item list-group-item-secondary">Description:</li>
-        <li class="list-group-item">{{description}}</li>
-        <li class="list-group-item list-group-item-secondary">Price:</li>
-        <li class="list-group-item">{{price}}</li>
-      </ul>
-      <b-button href="#" class="btn btn-secondary btn-lg btn-block btn-space">Buy Now</b-button>
-      <button type="button" class="btn btn-secondary btn-lg btn-block btn-space" onclick="addFavorite">Add to favorite</button>
+      <div class="col-sm">
+        <ul class="list-group card-space">
+          <li class="list-group-item list-group-item-secondary">Artist:</li>
+          <li class="list-group-item">{{ artistName }}</li>
+          <li class="list-group-item list-group-item-secondary">
+            Description:
+          </li>
+          <li class="list-group-item">{{ description }}</li>
+          <li class="list-group-item list-group-item-secondary">Price:</li>
+          <li class="list-group-item">{{ price }}</li>
+        </ul>
+        <b-button href="#" class="btn btn-secondary btn-lg btn-block btn-space"
+          >Buy Now</b-button
+        >
+        <button
+          type="button"
+          class="btn btn-secondary btn-lg btn-block btn-space"
+          onclick="addFavorite"
+        >
+          Add to favorite
+        </button>
+      </div>
     </div>
   </div>
-</div>
+</span>
 </template>
 
 <script>
@@ -69,25 +85,26 @@ export default {
         })
     },
     addFavorite: function () {
-    let userId = backend.retrieveCurrentUser().uid
-    if (userId == null) {
-        this.$alert("Sign in to add Artwork to your Favorites!")
-    } else {
-        // User id is therefore valid
-        let artlistingId = this.$route.params.id
-        backend.get('/customers/get/' + userId).then(function (response) {
-            // This function will run if the id is for a customer
-            return backend.post('/customers/add_favorite_listing/'+userId, {
-                'listingIdCode' : artlistingId
-            })
-        }).then(function(response){
-            // response.data is the newly updated CustomerDto object, if you need it
-        }).catch(function (error){
-            // This function will run if the id is not a valid customer,
-            // or just something goes wrong
-        })
+      let userId = backend.retrieveCurrentUser().uid
+      if (userId == null) {
+          this.$alert("Sign in to add Artwork to your Favorites!")
+      } else {
+          // User id is therefore valid
+          let artlistingId = this.$route.params.id
+          backend.get('/customers/get/' + userId).then(function (response) {
+              // This function will run if the id is for a customer
+              return backend.post('/customers/add_favorite_listing/'+userId, {
+                  'listingIdCode' : artlistingId
+              })
+          }).then(function(response){
+              // response.data is the newly updated CustomerDto object, if you need it
+          }).catch(function (error){
+              // This function will run if the id is not a valid customer,
+              // or just something goes wrong
+          })
+      }
     }
-},
+  },
   created: function () {
     this.getArt()
     this.getArtist()
