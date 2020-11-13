d020c37d5d973f1c0d7c6717e29d7f17c158af38
Ryan Au <auryan898@gmail.com>
2020-11-13 08:32:20 -0500

    cleared some useless comments in index.js

--- visart-web-app/src/router/index.js
+++ visart-web-app/src/router/index.js
@@ -23,33 +23,21 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/userpage/:id',
     name: 'UserPage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/UserPage.vue')
   },
   {
     path: '/artistpage/:id',
     name: 'ArtistPage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ArtistPage.vue')
   },
   {
     path: '/purchasepage/:id',
     name: 'PurchasePage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PurchasePage.vue')
   },
   {
