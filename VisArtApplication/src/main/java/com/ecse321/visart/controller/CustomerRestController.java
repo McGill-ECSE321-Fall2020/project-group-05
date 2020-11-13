2e71ba6fcad13379ecc4cc95f7b40edf0671d2d9
Ryan Au <auryan898@gmail.com>
2020-11-13 18:20:49 -0500

    Fix customer and manager rest, use user rest create

--- VisArtApplication/src/main/java/com/ecse321/visart/controller/CustomerRestController.java
+++ VisArtApplication/src/main/java/com/ecse321/visart/controller/CustomerRestController.java
@@ -165,7 +165,7 @@ public class CustomerRestController {
    */
   @PostMapping(value = { "/customers/create", "/customers/create/" })
   public CustomerDto createCustomer(@RequestBody MultiValueMap<String, String> values) {
-    return new CustomerDto(service.createCustomer(
+    return new CustomerDto(service.createCustomerUnified(
         values.getFirst("emailAddress"), values.getFirst("displayname"),
         values.getFirst("username"), values.getFirst("password"),
         values.getFirst("profilePicLink"), values.getFirst("profileDescription")));
