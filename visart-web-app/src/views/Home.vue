45db6891acdfe4c0df7a97edb0994f4e9118709c
Ryan Au <auryan898@gmail.com>
2020-11-13 14:53:33 -0500

    Fixed Home.vue

--- visart-web-app/src/views/Home.vue
+++ visart-web-app/src/views/Home.vue
@@ -1,127 +1,191 @@
 <template>
-
   <div class="home">
-  <section id="mainArtSection">
-    <hooper id="hooperContainer" :autoPlay="true" :playSpeed="7000" :itemsToShow="1" :infiniteScroll="true">
-    <slide>
-      <div class="sectionImage" id="mainHomeArt1"></div>
-    </slide>
-    <slide>
-      <div class="sectionImage" id="mainHomeArt2"></div>
-    </slide>
-    <slide>
-      <div class="sectionImage" id="mainHomeArt3"></div>
-    </slide>
-    <slide>
-      <div class="sectionImage" id="mainHomeArt4"></div>
-    </slide>
-    <hooper-navigation id="tagNavigation" slot="hooper-addons"></hooper-navigation>
-    </hooper>
-  <div class="sectionContent" id="sectionContentTitle">Vis Art</div>
-</section>
-<section id="tagSection">
-<div id="tagContainer">
- <hooper id="hooperContainerTags" :settings="hooperSettings">
-    <slide v-for="(tag, index) in tags" :key="index">
-    <button type="button" class="btn btn-primary tagBtn" @click="redirectToHome(tag.keyword)">{{tag.keyword}}</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Sculpture</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Cubism</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Realism</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Fauvism</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Oil</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Absurd</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Comtemporary</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Resin</button>
-    </slide>
-    <slide>
-    <button type="button" class="btn btn-primary tagBtn">Glass</button>
-    </slide>
-      <hooper-navigation slot="hooper-addons"></hooper-navigation>
-  </hooper>
-</div>
-</section>
+    <section id="mainArtSection">
+      <hooper
+        id="hooperContainer"
+        :autoPlay="true"
+        :playSpeed="7000"
+        :itemsToShow="1"
+        :infiniteScroll="true"
+      >
+        <slide>
+          <div class="sectionImage" id="mainHomeArt1"></div>
+        </slide>
+        <slide>
+          <div class="sectionImage" id="mainHomeArt2"></div>
+        </slide>
+        <slide>
+          <div class="sectionImage" id="mainHomeArt3"></div>
+        </slide>
+        <slide>
+          <div class="sectionImage" id="mainHomeArt4"></div>
+        </slide>
+        <hooper-navigation
+          id="tagNavigation"
+          slot="hooper-addons"
+        ></hooper-navigation>
+      </hooper>
+      <div class="sectionContent" id="sectionContentTitle">Vis Art</div>
+    </section>
+    <section id="tagSection">
+      <div id="tagContainer">
+        <hooper id="hooperContainerTags" :settings="hooperSettings">
+          <slide v-for="(tag, index) in tags" :key="index">
+            <button
+              type="button"
+              class="btn btn-primary tagBtn"
+              @click="redirectToHome(tag.keyword)"
+            >
+              {{ tag.keyword }}
+            </button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">
+              Sculpture
+            </button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">Cubism</button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">
+              Realism
+            </button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">
+              Fauvism
+            </button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">Oil</button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">Absurd</button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">
+              Comtemporary
+            </button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">Resin</button>
+          </slide>
+          <slide>
+            <button type="button" class="btn btn-primary tagBtn">Glass</button>
+          </slide>
+          <hooper-navigation slot="hooper-addons"></hooper-navigation>
+        </hooper>
+      </div>
+    </section>
     <div class="listingContainer">
       <label for="toggle_button">
-        <h1 v-if="isActive" id="listingsTitle"> – Featured Art – </h1>
-        <h1 v-if="! isActive" id="listingsTitle"> – Full Collection – </h1>
-        <button class="toggleBtnHome" @click="isActive = !isActive">{{isActive ? 'View Full Collection' : 'View Featured Art'}}</button>
+        <h1 v-if="isActive" id="listingsTitle">– Featured Art –</h1>
+        <h1 v-if="!isActive" id="listingsTitle">– Full Collection –</h1>
+        <button class="toggleBtnHome" @click="isActive = !isActive">
+          {{ isActive ? "View Full Collection" : "View Featured Art" }}
+        </button>
         <span class="toggle__switch"></span>
-    </label>
-      <div v-if="! isActive" class="card-columns card-columns-home">
-  <div class="card shadow homeCard" v-for="(artlisting,index) in artListingsFull" :key="index">
-    <hooper id="hooperContainer" :autoPlay="true" :playSpeed="10000" :itemsToShow="1" :infiniteScroll="true">
-      <slide v-for="(imageUrl,index) in artlisting.postImages" :key="index">
-        <img class="card-img-top cardImg" :src="imageUrl" alt="Card image cap">
-      </slide>
-      <hooper-navigation id="tagNavigation" slot="hooper-addons"></hooper-navigation>
-    </hooper>
-    <div class="sectionContent sectionContentListing">{{artlisting.title}}</div>
-    <div class="card-body">
-      <h4 class="card-title cardArtist"><a href="/artists">name : {{ getArtistNameMethod(artlisting.artist) }}</a></h4>
-      <h5 class="card-title cardPrice">$ {{artlisting.price}}</h5>
-      <div class="descriptionContainer">
-        <p class="card-text cardDesc">{{artlisting.description}}</p>
+      </label>
+      <div v-if="!isActive" class="card-columns card-columns-home">
+        <div
+          class="card shadow homeCard"
+          v-for="(artlisting, index) in artListingsFull"
+          :key="index"
+        >
+          <hooper
+            id="hooperContainer"
+            :autoPlay="true"
+            :playSpeed="10000"
+            :itemsToShow="1"
+            :infiniteScroll="true"
+          >
+            <slide
+              v-for="(imageUrl, index) in artlisting.postImages"
+              :key="index"
+            >
+              <img
+                class="card-img-top cardImg"
+                :src="imageUrl"
+                alt="Card image cap"
+              />
+            </slide>
+            <hooper-navigation
+              id="tagNavigation"
+              slot="hooper-addons"
+            ></hooper-navigation>
+          </hooper>
+          <div class="sectionContent sectionContentListing">
+            {{ artlisting.title }}
+          </div>
+          <div class="card-body">
+            <h4 class="card-title cardArtist">
+              <a href="/artists">name : {{ artlisting.artistName }}</a>
+            </h4>
+            <h5 class="card-title cardPrice">$ {{ artlisting.price }}</h5>
+            <div class="descriptionContainer">
+              <p class="card-text cardDesc">{{ artlisting.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="isActive" class="card-columns card-columns-home">
+        <div
+          class="card shadow homeCard"
+          v-for="(artlisting, index) in artListingsFeatured"
+          :key="index"
+        >
+          <img
+            class="card-img-top cardImg"
+            src="../assets/cardTrial.png"
+            alt="Card image cap"
+          />
+          <div class="sectionContent sectionContentListing">
+            {{ artlisting.title }}
+          </div>
+          <div class="card-body">
+            <h4 class="card-title cardArtist">
+              <a href="/artists">Picasso</a>
+            </h4>
+            <h5 class="card-title cardPrice">$ {{ artlisting.price }}</h5>
+            <div class="descriptionContainer">
+              <p class="card-text cardDesc">{{ artlisting.description }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-<div v-if="isActive" class="card-columns card-columns-home">
-  <div class="card shadow homeCard" v-for="(artlisting,index) in artListingsFeatured" :key="index">
-    <img class="card-img-top cardImg" src="../assets/cardTrial.png" alt="Card image cap">
-    <div class="sectionContent sectionContentListing">{{artlisting.title}}</div>
-    <div class="card-body">
-      <h4 class="card-title cardArtist"><a href="/artists">Picasso</a></h4>
-      <h5 class="card-title cardPrice">$ {{artlisting.price}}</h5>
-      <div class="descriptionContainer">
-        <p class="card-text cardDesc">{{artlisting.description}}</p>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-    <div>
-    </div>
+    <div></div>
   </div>
 </template>
 <script>
+/* eslint-disable */
 // @ is an alias to /src
 //  bar scroll
-import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper'
-import 'hooper/dist/hooper.css'
-import axios from 'axios'
-var config = require('../../config')
-var frontendUrl = config.site
-var backendUrl = config.backend.site
+import { Hooper, Slide, Navigation as HooperNavigation } from "hooper";
+import "hooper/dist/hooper.css";
+import axios from "axios";
+var config = require("../../config");
+var frontendUrl = config.site;
+var backendUrl = config.backend.site;
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl, 'Content-Type': 'raw', 'Data-Type': 'raw' }
-})
-console.log('im here')
+  headers: {
+    "Access-Control-Allow-Origin": frontendUrl,
+    "Content-Type": "raw",
+    "Data-Type": "raw"
+  }
+});
+console.log("im here");
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Hooper,
     Slide,
     HooperNavigation
   },
-  data () {
+  data() {
     return {
       isActive: false,
       tags: [],
@@ -134,62 +198,55 @@ export default {
         infiniteScroll: true,
         itemsToSlide: 1,
         rtl: false,
-        pagination: 'no',
+        pagination: "no",
         mouseDrag: false,
         autoPlay: true
-      }
-    }
+      },
+      artistNames: {}
+    };
   },
-  created: function () {
-    AXIOS.get('/artlisting/get_all').then(response => {
-      console.log(response.data)
-      for (const artListing of (response.data)) {
-        this.artListingsFull.push(artListing)
-        for (const imageUrl of (artListing.postImages)) {
-          this.postImagesArray.push(imageUrl)
+  created: function() {
+    let vm = this;
+    AXIOS.get("/artlisting/get_all")
+      .then(response => {
+        console.log(response.data);
+        for (const artListing of response.data) {
+          AXIOS.get("artists/get/" + artListing.artist).then(response => {
+            artListing.artistName = response.data.customer.user.displayname;
+            console.log(artListing.artistName);
+            vm.artListingsFull.push(artListing);
+            for (const imageUrl of artListing.postImages) {
+              vm.postImagesArray.push(imageUrl);
+            }
+          });
         }
-      }
-    }).catch(e => {
-      console.log(e)
-    })
-    AXIOS.get('/managers/get_listings/').then(response => {
-      console.log(response.data)
-      for (const managerListing of (response.data)) {
-        this.artListingsFeatured.push(managerListing)
-      }
-    }).catch(e => {
-      console.log(e)
-    })
-    AXIOS.get('/tags/get_all').then(response => {
-      console.log(response.data)
-      for (const tag of (response.data)) {
-        this.tags.push(tag)
-      }
-    }).catch(e => {
-      console.log(e)
-    })
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    // AXIOS.get('/managers/get_listings/'+managerId).then(response => { // TODO: needs managerId
+    //   console.log(response.data)
+    //   for (const managerListing of (response.data)) {
+    //     this.artListingsFeatured.push(managerListing)
+    //   }
+    // }).catch(e => {
+    //   console.log(e)
+    // })
+    AXIOS.get("/tags/get_all")
+      .then(response => {
+        console.log(response.data);
+        for (const tag of response.data) {
+          this.tags.push(tag);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
   },
   methods: {
-    redirectToHome: function (tag) {
-      this.$router.push({ path: '/search/?keywords=' + tag })
-    },
-    getArtistNameMethod: async function (id) {
-      return await AXIOS
-        .get(
-          '/artists/get/' + id
-        )
-        .then(
-          response => {
-            console.log(response.data)
-            console.log(response.data.customer.user.displayname)
-            return response.data.customer.user.displayname
-          }
-        )
-        .catch(e => {
-          console.log(e)
-          return e
-        })
+    redirectToHome: function(tag) {
+      this.$router.push({ path: "/search/?keywords=" + tag });
     }
   }
-}
+};
 </script>
