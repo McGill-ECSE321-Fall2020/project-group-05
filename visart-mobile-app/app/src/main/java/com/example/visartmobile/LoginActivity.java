9c6a5cac445b9efcc7f627e117c89235b8e624b5
Ryan Au <auryan898@gmail.com>
2020-11-19 21:18:29 -0500

    Creates a user from the login screen

--- visart-mobile-app/app/src/main/java/com/example/visartmobile/LoginActivity.java
+++ visart-mobile-app/app/src/main/java/com/example/visartmobile/LoginActivity.java
@@ -2,11 +2,23 @@ package com.example.visartmobile;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
+    public static final String PLACEHOLDER_PROFILE_PIC = "https://firebasestorage.googleapis.com/v0/b/visartapplication.appspot.com/o/profile-placeholder.png?alt=media&token=8c962ff5-4177-4317-81ff-d6d54eaaf2f7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,45 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginButtonClicked(View view) {
-        Toast.makeText(LoginActivity.this, "Email or Password Incorrect ", Toast.LENGTH_LONG).show();
+        EditText emailBox = findViewById(R.id.loginEmail);
+        String randomNumbers = (System.nanoTime() + "");
+        randomNumbers = randomNumbers.substring(randomNumbers.length() - 5);
+        String[][] data = {
+                {"emailAddress", randomNumbers + "bob@email.com"},
+                {"displayname", randomNumbers + "TheRy"},
+                {"username", randomNumbers + "TheRy"},
+                {"password", "password"},
+                {"profilePicLink", PLACEHOLDER_PROFILE_PIC},
+                {"profileDescription", "I describe myself here."},
+        };
+        HttpUtils.postForm("users/create", data, new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Toast.makeText(LoginActivity.this, "Email or Password Incorrect ", Toast.LENGTH_LONG).show();
+                System.err.println(e.toString());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                JSONObject json = null;
+                try {
+                    String resp = response.body().string();
+                    json = new JSONObject(resp);
+
+                    System.out.println(json.toString());
+                    final String name = json.getString("username");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(LoginActivity.this, "Login Successful, Welcome " + name, Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
