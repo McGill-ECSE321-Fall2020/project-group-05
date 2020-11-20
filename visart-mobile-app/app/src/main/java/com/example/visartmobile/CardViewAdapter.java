1708f41b41c97c9ae2346e18811b86a31c7df4b4
Daniel Bucci <danielbucci2@gmail.com>
2020-11-19 20:53:48 -0500

    ui changes to recycler view on Main Activty

--- visart-mobile-app/app/src/main/java/com/example/visartmobile/CardViewAdapter.java
+++ visart-mobile-app/app/src/main/java/com/example/visartmobile/CardViewAdapter.java
@@ -18,6 +18,19 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
         this.artListings = artListings;
     }
 
+    /***** Creating OnItemClickListener *****/
+
+    // Define listener member variable
+    private OnItemClickListener listener;
+    // Define the listener interface
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
+    // Define the method that allows the parent activity or fragment to define the listener
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -33,6 +46,20 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
             super(itemView);
 
             cardTitle = (TextView) itemView.findViewById(R.id.card_name);
+
+            // Setup the click listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Triggers click upwards to the adapter on click
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(itemView, position);
+                        }
+                    }
+                }
+            });
         }
     }
 
