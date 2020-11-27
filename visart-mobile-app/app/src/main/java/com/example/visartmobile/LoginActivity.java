a8deccbfdb5955e5626ffa21aef36fac59e2d040
yuterzbell <yuta.bellali@mail.mcgill.ca>
2020-11-27 16:30:51 -0500

    at success page, add ability to display name

--- visart-mobile-app/app/src/main/java/com/example/visartmobile/LoginActivity.java
+++ visart-mobile-app/app/src/main/java/com/example/visartmobile/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.visartmobile.util.UserAuth;
